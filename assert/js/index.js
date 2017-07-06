35060b480589bab725937a960602cc01ec68be50
bayeszou <bayeszou@gmail.com>
2017-07-06 12:02:24 +0800

    add tabs

--- assert/js/index.js
+++ assert/js/index.js
@@ -1,11 +1,22 @@
-$(()=>{
-  $('.site-nav').on('click', 'li', e=>{
+$(() => {
+  $('.site-nav').on('click', 'li', e => {
     let $li = $(e.currentTarget)
     let index = $li.index()
     if (index === 1) {
-
-    } else if (index ===2) {
-
+      if ($li.attr('data-downloaded') != true) {
+        console.log($li)
+        $li.attr('data-downloaded', 'true')
+      }
+    } else if (index === 2) {
+      if ($li.attr('data-downloaded') != true) {
+        console.log($li)
+        $li.attr('data-downloaded', 'true')
+      }
     }
+    $li.addClass('active').siblings().removeClass('active')
+    $('.container > div').eq(index).addClass('active').siblings().removeClass('active')
+  })
+  $.get('/a.json', data=>{
+    console.log(data)
   })
 })
