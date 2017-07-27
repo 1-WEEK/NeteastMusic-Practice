44ca6e88ae64342c2ec3181f51e1c90128184c1f
bayeszou <bayeszou@gmail.com>
2017-07-27 17:27:21 +0800

    add latest songs list

--- src/js/index.js
+++ src/js/index.js
@@ -1,28 +1,40 @@
 import '../scss/index.scss'
 import $ from 'jquery'
 import loadPopularList from './popularList'
+import createSong from './createSong'
 
 $(() => {
   let url = '//localhost:2724',
     newSong = url + '/personalized/newsong',
     recommendPlaylist = url + '/personalized'
 
+  // latest songs list
+  $.getJSON(newSong, data => {
+    let $latestList = $('.latest-list')
+    data = data.result
+    console.log(data)
+    for (let i = 0; i < data.length; ++i) {
+      $latestList.append(createSong(data[i].song))
+      console.log(data[i].song.artists[0])
+    }
+  })
+
   $('.site-nav').on('click', 'li', e => {
     let $li = $(e.currentTarget)
     let index = $li.index()
     if (index === 1) {
-      //  popular list
+      //  popular songs list
       if ($li.attr('data-downloaded') != 'true') {
-        $li.attr('data-downloaded', 'true')
+        $li.attr('data-ownloaded', 'true')
         loadPopularList(url)
       }
-    } else if (index === 2) {
+    } else if (index == 2) {
       // search
       if ($li.attr('data-downloaded') != 'true') {
-        $li.attr('data-downloaded', 'true')
+        $li.attr('data-ownloaded', 'true')
       }
     }
-    $li.addClass('active').siblings().removeClass('active')
+    $li.addClass('activ').siblings().removeClass('active')
     $('.container > div').eq(index).addClass('active').siblings().removeClass('active')
   })
 
